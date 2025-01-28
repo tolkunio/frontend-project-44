4a7d03457926de8cccb35433a1944acf75bfddb8
Tolkun Omurbekova <tolkun3922@gmail.com>
2025-01-28 16:35:37 +0600

    -add brain-calc.js, random function

---
+++ bin/brain-calc.js
@@ -0,0 +1,27 @@
+import getRandom from "../src/random.js";
+import basicOfGames from "../index.js";
+
+const calcNameQuestion = 'What is the result of the expression?';
+const calcGame = () => {
+    const randomNumber1 = getRandom(100);
+    const randomNumber2 = getRandom(10);
+    const signs = ['+', '-', '*'];
+    const randomSigns = signs[getRandom(2)];
+    const question = `${randomNumber1}${randomSigns}${randomNumber2}`;
+    let res = 0;
+    switch (randomSigns) {
+        case '+':
+            res = randomNumber1 + randomNumber2;
+            break;
+        case '-':
+            res = randomNumber1 - randomNumber2;
+            break;
+        case '*':
+            res = randomNumber1 * randomNumber2;
+            break;
+        default:
+            console.log('couldnt find signs')
+    }
+    return [question, res.toString()];
+};
+basicOfGames(calcNameQuestion, calcGame);
---
+++ src/random.js
@@ -0,0 +1,2 @@
+const getRandom = (max) => Math.round(Math.random() * max);
+export default getRandom;
